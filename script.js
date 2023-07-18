72d3dfa4c6f3991017c99afaa09c9604a289b029
AndreaLinza <andrealinza@gmail.com>
2023-07-18 23:56:24 +0200

    completed carousel

--- script.js
+++ script.js
@@ -22,143 +22,210 @@ const images = [
     }
 ];
 
-const containerCarosel = document.querySelector(".container-carosel")
-
-//MAIN
-const mainContainerCarosel = document.createElement("div")
-mainContainerCarosel.classList.add("main-container-carosel")
-
-const mainImgCarosel = document.createElement("div")
-mainImgCarosel.classList.add("main-img-carosel")
-
-const descriptionCarosel = document.createElement("p")
-descriptionCarosel.classList.add("description-carosel")
 
 
-mainContainerCarosel.append(mainImgCarosel)
-containerCarosel.append(mainContainerCarosel)
-
-//ASIDE
-const asideContainerCarosel = document.createElement("div")
-asideContainerCarosel.classList.add("aside-container-carosel")
-
-const asideImgCarosel = document.createElement("div")
-asideImgCarosel.classList.add("aside-img-carosel")
-
-asideContainerCarosel.append(asideImgCarosel)
-containerCarosel.append(asideContainerCarosel)
+const mainContainer = document.querySelector(".carousel-container");
+const thumbsContainer = document.querySelector(".carousel-thumbs-container");
+const btnNext = document.getElementById("next-btn");
+const btnPrev = document.getElementById("prev-btn");
+let imgIndex = 0;
+let imagesListEl = []
+let thumbsListEl = []
 
 
-// images.forEach(element => {
-//     mainImgCarosel.innerText += `${element.title} ${element.text}`
-//     mainImgCarosel.innerHTML += `<img src="${element.image}" class="carosel-img-1" alt="">`
-//     asideImgCarosel.innerHTML += `<img src="${element.image}" class="carosel-img-2" alt="">`
+images.forEach((element, i) => {
+
+    const div = document.createElement("div")
+    div.classList.add("carousel-image-container")
+    div.innerHTML = `<img src="${element.image}" class="carousel-image" alt="">
+    <div class="container-description">
+    <p class="fw-bold fs-4 mb-2">${element.title}</p>
+    <div class="w-75 ms-auto">
+    <p class="text-carousel fs-6 ">${element.text}</p>
+    </div>
+    </div>`
+
+    if (i === imgIndex) {
+        div.classList.add("active");
+        const bgDescription = document.createElement("div")
+        bgDescription.innerHTML = ``
+    }
+
+    mainContainer.append(div);
+
+    imagesListEl.push(div);
+
+});
+
+
+images.forEach((element, i) => {
+    const div = document.createElement("div");
+    div.classList.add("carousel-img-thumb-container", "d-flex");
+    div.innerHTML = `<img src="${element.image}" alt="" class="carousel-img-thumb">`;
+
+    if (i === imgIndex) {
+        div.classList.add("active");
+    }
+
+    thumbsContainer.append(div);
+
+    thumbsListEl.push(div);
+
+});
+
+console.log(imagesListEl)
+
+
+
+btnNext.addEventListener("click", function () {
+
+    imagesListEl[imgIndex].classList.remove("active");
+    thumbsListEl[imgIndex].classList.remove("active");
+
+
+    if (imgIndex < 4) {
+
+        imgIndex++;
+    }
+    else {
+        imgIndex = 0
+    }
+
+    imagesListEl[imgIndex].classList.add("active");
+    thumbsListEl[imgIndex].classList.add("active");
+})
+
+
+btnPrev.addEventListener("click", function () {
+
+    imagesListEl[imgIndex].classList.remove("active");
+    thumbsListEl[imgIndex].classList.remove("active");
+    if (imgIndex > 0) {
+
+        imgIndex--
+    }
+    else {
+        imgIndex = 4
+    }
+
+
+    imagesListEl[imgIndex].classList.add("active");
+    thumbsListEl[imgIndex].classList.add("active");
+})
+
+
+// const mainContainer = document.querySelector(".container")
+// const containerCarosel = document.createElement("div")
+// containerCarosel.classList.add("container-carosel", "m-auto", "d-flex")
+
+// const mainContainerCarosel = document.createElement("div")
+// mainContainerCarosel.classList.add("main-container-carosel")
+
+
+// const mainImgCarosel = document.createElement("div")
+// mainImgCarosel.classList.add("main-img-carosel")
+
+
+// const asideContainerCarosel = document.createElement("div")
+// asideContainerCarosel.classList.add("aside-container-carosel")
+
+// const asideImgCarosel = document.createElement("div")
+// asideImgCarosel.classList.add("aside-img-carosel")
+
+
+
+// mainContainerCarosel.append(mainImgCarosel)
+// asideContainerCarosel.append(asideImgCarosel)
+// containerCarosel.append(mainContainerCarosel, asideContainerCarosel)
+// mainContainer.append(containerCarosel)
+
+
+
+// // images.forEach((singleGame, i) => {
+// for (let key in images) {
+
+
+
+//     mainImgCarosel.innerHTML +=
+//         `<div class="main-img-carosel">
+//     <img src="${images[key].image}" class="carosel-img" alt="">
+//     <div class="description-container">
+//     <p class="description-title mt-3 mb-1">${images[key].title}</p>
+//     <span class="description-game">${images[key].text}</span>
+//     </div>
+//     </div>
+//     `
+
+
+//     //asideImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img-thumb" alt="">`
+
+// };
+
+
+
+// const mainContainer = document.querySelector(".container")
+// const containerCarosel = document.createElement("container-carosel")
+// mainContainer.classList.add("m-auto", "d-flex")
+
+
+// //MAIN
+// const mainContainerCarosel = document.createElement("div")
+// mainContainerCarosel.classList.add("main-container-carosel")
+
+// const mainImgCarosel = document.createElement("div")
+// mainImgCarosel.classList.add("main-img-carosel")
+
+// const descriptionCarosel = document.createElement("p")
+// descriptionCarosel.classList.add("description-container")
+
+// //DESCRIPTION
+// const containerDescriptionImg = document.createElement("div")
+// const descriptionImg = document.createElement("p")
+// descriptionImg.classList.add("description-title")
+
+
+
+
+
+
+
+// //BUTTONS
+// const prevBtn = document.getElementById("btn-prev")
+// const nextBtn = document.getElementById("btn-next")
+
+
+
+// containerDescriptionImg.append(descriptionImg)
+// mainContainerCarosel.append(mainImgCarosel,containerDescriptionImg)
+// containerCarosel.append(mainContainerCarosel)
+
+
+// //ASIDE
+// const asideContainerCarosel = document.createElement("div")
+// asideContainerCarosel.classList.add("aside-container-carosel")
+
+
+
+// const asideImgCarosel = document.createElement("div")
+// asideImgCarosel.classList.add("aside-img-carosel")
+
+// asideContainerCarosel.append(asideImgCarosel)
+// containerCarosel.append(asideContainerCarosel)
+
+
+// images.forEach(singleImg => {
+//     mainImgCarosel.innerText += `${singleImg.title} ${singleImg.text}`
+//     mainImgCarosel.innerHTML += `<img src="${singleImg.image}" class="carosel-img-1" alt="">`
+//     asideImgCarosel.innerHTML += `<img src="${singleImg.image}" class="carosel-img-2" alt="">`
 // });
 
 
+// for (let key in images) {
 
-for (let key in images) {
-    mainImgCarosel.innerText += `${images[key].title} ${images[key].text}`
-    mainImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img-1" alt="">`
-    asideImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img-2" alt="">`
-
+//     mainImgCarosel.innerText += `${images[key].title} ${images[key].text}`
 //     mainImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img" alt="">`
+//     asideImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img-thumb" alt="">`
 
-}
+    //     mainImgCarosel.innerHTML += `<img src="${images[key].image}" class="carosel-img" alt="">`
 
-
-
-// const imagesList = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
-// const carouselContainer = document.querySelector(".my-carousel-container");
-// let currentImageIndex = 0; // valore iniziale
-
-// for (let i = 0; i < imagesList.length; i++) {
-//   // recupero l'immagine all'indice i
-//   const currentImage = imagesList[i];
-//   let imageClasses = "";
-
-//   // i === 0 -> al primo giro del ciclo | alla prima immagine della lista
-//   if (i === 0) {
-//     // sto analizzando la prima immagine
-//     imageClasses = "active";
-//   }
-
-//   /* Quando usiamo innerHTML, il codice all'interno di quell'elemento viene riscritto ogni volta che
-//   concateniamo del nuovo html, usando +=
-//   Siccome il codice viene riscritto, gli eventuali collegamenti a quegli elementi vengono persi e JS
-//   non riesce a ricollegarsi dopo che l'html viene riscritto */
-//   carouselContainer.innerHTML += `<div class="my-carousel-image-container ${imageClasses}">
-//   <img src="imgs/${currentImage}" alt="" class="my-carousel-image">
-// </div>`;
-
-//   /* carouselContainer.innerHTML +=
-//     '<div class="my-carousel-image-container">' +
-//     '<img src="imgs/' + currentImage + '" alt="" class="my-carousel-image">' +
-//     "</div>"; */
-// }
-
-// const prevBtn = document.getElementById("carousel-prev-btn");
-// const nextBtn = document.getElementById("carousel-next-btn");
-
-// nextBtn.addEventListener("click", function () {
-//   console.log("cliccato pulsante NEXT");
-
-//   // recupero TUTTi gli elementi che hanno la classe .my-carousel-image-container
-//   // array di HTMLElement
-//   const imageListElements = document.querySelectorAll(
-//     ".my-carousel-image-container"
-//   );
-
-//   console.log(imageListElements[currentImageIndex]);
-
-//   // dall'elemento attualmetne attivo, tolgo classe active
-//   imageListElements[currentImageIndex].classList.remove("active");
-
-//   // Incremento il contatore globale
-//   currentImageIndex++;
-
-//   if (currentImageIndex > imageListElements.length - 1) {
-//     currentImageIndex = 0;
-//   }
-
-//   // Siccome ora currentImageIndex ha un numero diverso da prima, lo uso per assegnare la classe active
-//   imageListElements[currentImageIndex].classList.add("active");
-
-//   // prendo l'elemento subito dopo quello con classe active
-//   // const nextEl = document.querySelector(".my-carousel-image-container.active + div");
-//   // const nextEl = document.querySelector(".my-carousel-image-container.active + div");
-
-//   // Tolgo la classe .active dall'elemento che ha questa classe
-//   // document.querySelector(".my-carousel-image-container.active").classList.remove("active");
-
-//   // prendere l'elemento successivo a quello che aveva la classe active,
-//   // e mettere la classe su questo nuovo elemento
-
-//   // aggiungo active sul nuovo elemento
-//   // nextEl.classList.add("active");
-// });
-
-// prevBtn.addEventListener("click", function () {
-//   console.log("cliccato pulsante PREVIOUS");
-
-//   // recupero TUTTi gli elementi che hanno la classe .my-carousel-image-container
-//   // array di HTMLElement
-//   const imageListElements = document.querySelectorAll(
-//     ".my-carousel-image-container"
-//   );
-
-//   console.log(imageListElements[currentImageIndex]);
-
-//   // dall'elemento attualmetne attivo, tolgo classe active
-//   imageListElements[currentImageIndex].classList.remove("active");
-
-//   // Decremento il contatore globale
-//   currentImageIndex--;
-
-//   if(currentImageIndex < 0){
-//     currentImageIndex = imageListElements.length - 1;
-//   }
-
-//   // Siccome ora currentImageIndex ha un numero diverso da prima, lo uso per assegnare la classe active
-//   imageListElements[currentImageIndex].classList.add("active");
-// });
+//}
